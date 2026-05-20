13af7fcc86c945130145ff46f02c3cbb788421e3
Daniel Guimarães dos Santos <cin0235882@aluno.firjansenaisesi.com.br>
2026-05-20 20:17:36 -0300

    adicao RN01

--- src/services/ProdutoServices.ts
+++ src/services/ProdutoServices.ts
@@ -1,8 +1,9 @@
 import { AppDataSource } from "../data-source.js";
-import { Produtos } from "../entity/Produtos.js";
-
+import { Produtos, Status } from "../entity/Produtos.js";
+import { ConflictError, NotFoundError } from "../helpers/apiError.js";
 export class ProdutoService {
   private produtoRepository = AppDataSource.getRepository(Produtos);
+  
 
   create = async (prod: Partial<Produtos>) => {
     const nProd = this.produtoRepository.create(prod);
@@ -16,16 +17,22 @@ export class ProdutoService {
   update = async (id: number, dados: Partial<Produtos>) => {
     const produto = await this.produtoRepository.findOneBy({ id });
     if (!produto) {
-      throw new Error("Produto não encontrado");
+      throw new NotFoundError("Produto não encontrado");
     }
     const produtoAtualizado = this.produtoRepository.merge(produto, dados);
     return await this.produtoRepository.save(produtoAtualizado);
   };
 
   delete = async (id: number) => {
-    const produto = await this.produtoRepository.findOneBy({ id });
-    if (!produto) throw new Error("produto não encontrado");
+    const produtoId = await this.produtoRepository.findOneBy({ id });
+    
+    if (!produtoId) throw new NotFoundError("produto não encontrado");
+    if (produtoId.status === Status.ALU) {
+      throw new ConflictError("Produto alugado, não pode ser deletado");
+    }
     await this.produtoRepository.delete(id);
     return { message: "Produto deletado com sucesso" };
+    
   };
+
 }
