4dbade0a9b305964f1c4a6f5e41b08a05895687d
Daniel Guimarães dos Santos <cin0235882@aluno.firjansenaisesi.com.br>
2026-05-20 19:27:51 -0300

    criacao do authController

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,36 +1,10 @@
-import { AuthService } from "../services/AuthService.js";
-import { Router, Request, Response, NextFunction } from "express";
+import { AuthController } from "../controller/AuthController.js";
+import {Router, } from "express";
 
 const router = Router();
-const authService = new AuthService();
+const authController = new AuthController();
 
-router.post(
-  "/registrar",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const funcionario = await authService.registrar(req.body);
-      res.status(201).json({
-        id: funcionario.id,
-        email: funcionario.email,
-        role: funcionario.role,
-      });
-    } catch (error) {
-      next(error);
-    }
-  },
-);
-
-router.post(
-  "/logar",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { email, senha } = req.body;
-      const result = await authService.logar(email, senha);
-      res.status(200).json(result);
-    } catch (error) {
-      next(error);
-    }
-  },
-);
+router.post("/",authController.registrar);
+router.post("/",authController.logar);
 
 export const authRoutes = router;
